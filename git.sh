753c3991d36c85acd1cb796de8d0ea580efa2c42
Gabriel Damon <gabrieldamon42@gmail.com>
2014-05-22 21:48:10 -0700

    better and better

--- git.sh
+++ git.sh
@@ -2,6 +2,7 @@
 git add --all
 git commit
 git push
+BRANCH=$(git rev-parse --abbrev-ref HEAD)
 MESSAGE=$(git log -1 --pretty=%B)
 echo $MESSAGE
-ssh ben42picow@gabriel-damon.com "./wp-cli.phar post create --post_content='$MESSAGE' --post_title='git push' --post_status=publish --path='public_html/ben/'"
+ssh ben42picow@gabriel-damon.com "./wp-cli.phar post create --post_content='$MESSAGE' --post_title='git push origin $BRANCH' --post_status=publish --path='public_html/ben/'"
