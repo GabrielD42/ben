45021aa450ea6f8782a6e521130cbb4cfcc025b8
Gabriel Damon <gabrieldamon42@gmail.com>
2014-05-22 21:48:49 -0700

    stuff

--- git.sh
+++ git.sh
@@ -4,5 +4,4 @@ git commit
 git push
 BRANCH=$(git rev-parse --abbrev-ref HEAD)
 MESSAGE=$(git log -1 --pretty=%B)
-echo $MESSAGE
 ssh ben42picow@gabriel-damon.com "./wp-cli.phar post create --post_content='$MESSAGE' --post_title='git push origin $BRANCH' --post_status=publish --path='public_html/ben/'"
