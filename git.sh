0851eb8c58218515fef394415d6e28ea3c285d55
Gabriel Damon <gabrieldamon42@gmail.com>
2014-05-22 21:43:20 -0700

    hopefully first commit like this

--- git.sh
+++ git.sh
@@ -2,6 +2,6 @@
 git add --all
 git commit
 git push
-MESSAGE=(git log -1 --pretty=%B)
+MESSAGE=$(git log -1 --pretty=%B)
 echo $MESSAGE
-ssh ben42picow@gabriel-damon.com "wp post create $MESSAGE --post_title='git push' --post_status=publish"
+ssh ben42picow@gabriel-damon.com "./wp-cli.phar post create $MESSAGE --post_title='git push' --post_status=publish"
