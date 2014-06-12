fdcf5ebff6c1d035705e4e22e97dc31bd274e3c0
Gabriel Damon <gabrieldamon42@gmail.com>
2014-06-12 16:11:59 -0700

    changes

--- git.sh
+++ git.sh
@@ -2,6 +2,6 @@
 git add --all
 git commit
 git push
-BRANCH=$(git rev-parse --abbrev-ref HEAD)
-MESSAGE=$(git log -1 --pretty=%B)
-ssh ben42picow@gabriel-damon.com "./wp-cli.phar post create --post_content=$MESSAGE --post_title='git push origin $BRANCH' --post_status=publish --path='./public_html/ben/'"
+#BRANCH=$(git rev-parse --abbrev-ref HEAD)
+#MESSAGE=$(git log -1 --pretty=%B)
+#ssh ben42picow@gabriel-damon.com "./wp-cli.phar post create --post_content=$MESSAGE --post_title='git push origin $BRANCH' --post_status=publish --path='./public_html/ben/'"
