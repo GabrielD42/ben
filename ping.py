66672ed4c41f51554449f1bdbe6cae7bc687932a
Gabriel Damon <gabrieldamon42@gmail.com>
2014-06-18 18:08:24 -0700

    stupid formatting...

--- ping.py
+++ ping.py
@@ -1,29 +1,29 @@
 import RPi.GPIO as GPIO
-import time.sleep as sleep
+import time.sleep
 
 triggerPin = 24
 echoPin = 25
 
 def setup():
-	GPIO.setmode(GPIO.BCM)
-	GPIO.setup(echoPin, GPIO.IN)
-	GPIO.setup(triggerPin, GPIO.OUT)
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(echoPin, GPIO.IN)
+    GPIO.setup(triggerPin, GPIO.OUT)
 
 def loop():
-	GPIO.output(triggerPin, GPIO.LOW)
-	sleep(0.000002)
-	GPIO.output(triggerPin, GPIO.HIGH)
-	sleep(0.000005)
-	GPIO.output(triggerPin, GPIO.LOW)
-	start = time.time()
-	while GPIO.input(GPIO_ECHO)==0:
-	  start = time.time()
-	while GPIO.input(GPIO_ECHO)==1:
-	  stop = time.time()
-	elapsed = stop-start
-	print("Distance: " + elapsed / 74 / 2)
+    GPIO.output(triggerPin, GPIO.LOW)
+    time.sleep(0.000002)
+    GPIO.output(triggerPin, GPIO.HIGH)
+    time.sleep(0.000005)
+    GPIO.output(triggerPin, GPIO.LOW)
+    start = time.time()
+    while GPIO.input(echoPin) == 0:
+        start = time.time()
+    while GPIO.input(echoPin) == 1:
+        stop = time.time()
+    elapsed = stop - start
+    print("Distance: " + elapsed / 74 / 2)
 
 #main
 setup()
 while True:
-	loop()
+    loop()
