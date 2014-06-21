7d3b04ba7f013dd62ea993f6365202c17f3ab537
Gabriel Damon <gabrieldamon42@gmail.com>
2014-06-21 13:52:30 -0700

    changes

--- ping.cpp
+++ ping.cpp
@@ -9,6 +9,9 @@ using namespace std;
 const int echoPin = 25;
 const int triggerPin = 24;
 
+long microsecondsToInches(long microseconds);
+long microsecondsToCentimeters(long microseconds);
+
 void setup() {
 	pinMode(triggerPin, OUTPUT);
 	pinMode(echoPin, INPUT);
@@ -28,7 +31,7 @@ void loop() {
   // The echo pin is used to read the signal from the PING))): a HIGH
   // pulse whose duration is the time (in microseconds) from the sending
   // of the ping to the reception of its echo off of an object.
-  duration = pulseIn(echoPin, HIGH);
+  duration = pulseIn(echoPin, HIGH, 100000L);
 
   // convert the time into a distance
   inches = microsecondsToInches(duration);
@@ -41,6 +44,57 @@ void loop() {
 
 void cleanup() {
 	// nothing to do
+	}
+
+long microsecondsToInches(long microseconds)	{
+  // According to Parallax's datasheet for the PING))), there are
+  // 73.746 microseconds per inch (i.e. sound travels at 1130 feet per
+  // second).  This gives the distance travelled by the ping, outbound
+  // and return, so we divide by 2 to get the distance of the obstacle.
+  // See: http://www.parallax.com/dl/docs/prod/acc/28015-PING-v1.3.pdf
+  return microseconds / 74 / 2;
+	}
+
+long microsecondsToCentimeters(long microseconds)	{
+  // The speed of sound is 340 m/s or 29 microseconds per centimeter.
+  // The ping travels out and back, so to find the distance of the
+  // object we take half of the distance travelled.
+  return microseconds / 29 / 2;
+	}
+
+int pulseIn(int pin, int level, int timeout)	{
+	struct timeval tn, t0, t1;
+  long micros;
+
+  gettimeofday(&t0, NULL);
+
+  micros = 0;
+  while (digitalRead(pin) != level)	{
+	  gettimeofday(&tn, NULL);
+
+    if (tn.tv_sec > t0.tv_sec) micros = 1000000L; else micros = 0;
+	    micros += (tn.tv_usec - t0.tv_usec);
+
+    if (micros > timeout)
+    	return 0;
+   	}
+
+  gettimeofday(&t1, NULL);
+
+  while (digitalRead(pin) == level)	{
+		gettimeofday(&tn, NULL);
+
+    if (tn.tv_sec > t0.tv_sec) micros = 1000000L; else micros = 0;
+  	  micros = micros + (tn.tv_usec - t0.tv_usec);
+
+    if (micros > timeout)
+    	return 0;
+   	}
+
+  if (tn.tv_sec > t1.tv_sec) micros = 1000000L; else micros = 0;
+  micros = micros + (tn.tv_usec - t1.tv_usec);
+
+  return micros;
 	}
 
 /////////////////////////////////////////////
@@ -66,7 +120,7 @@ int main() {
 			loop();
 			}
 
-		exit(0) // return with errors (this should never be reached)
+		exit(0); // return with errors (this should never be reached)
 		}
 	else {
 		cout << "wiringPi setup failed" << endl;
