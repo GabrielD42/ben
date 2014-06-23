6f5ed43e6ac1ed796ee19a233926854f4e389da8
Gabriel Damon <gabrieldamon42@gmail.com>
2014-06-23 15:53:13 -0700

    same

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
-echo "Uploading to Github..."
+
+# color variables for output (see https://gist.github.com/GabrielD42/9de9668486ef0b642b48)
+green='\e[0;32m'
+NC='\e[0m' # No Color
+
+echo -e "${green}Uploading to Github...${NC}"
 git add --all
 git commit
 git push
-echo "Pi downloading and compiling code..."
-ssh pi@192.168.0.51 '~/updategit.sh; ~/compile.sh'
+echo -e "${green}Pi downloading and compiling code...${NC}"
+ssh pi@192.168.0.51 '~/pullchanges.sh; ~/compile.sh'
