2b1989f9c394223c60797a8ff6bc067f6cd2860a
Gabriel Damon <gabrieldamon42@gmail.com>
2014-06-23 17:53:02 -0700

    actual code!

--- ping/main.cpp
+++ ping/main.cpp
@@ -1,15 +1,20 @@
 #include "main.h"
 #include "ping.h"
 #include "base.h"
+#include <iostream>
+#include <wiringPi.h>
 
 using namespace std;
 
-void setup() {
+Ping sensor(25, 25);
 
+void setup() {
+	Ping.init();
 	}
 
 void loop() {
-
+	cout << sensor.getInches() << "in" << endl;
+	delay(1000);
 	}
 
 void cleanup() {
