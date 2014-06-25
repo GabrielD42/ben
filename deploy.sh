1f9c1cba2a2a5e3a8afe785c907a7390ae70de5b
HelloKitty7 <thisaintforuruse@gmail.com>
2014-06-24 17:55:42 -0700

    changes

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,7 @@ NC='\e[0m' # No Color
 
 echo -e "\n${green}********************************\n* Uploading new code to Github *\n********************************${NC}\n"
 git add --all
-git commit
+git commit -m $1
 git push
 echo -e "\n${green}*************************************\n* Pi downloading and compiling code *\n*************************************${NC}\n"
 ssh pi@192.168.0.51 '~/pullchanges.sh; make clean; make'
