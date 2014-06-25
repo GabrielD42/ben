abc9f070ae691912c101b43fe3dc98e03f2fcc3d
Gabriel Damon <gabrieldamon42@gmail.com>
2014-06-24 21:07:08 -0700

    -m

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,33 @@
 #!/bin/bash
 
+# color variables for output (see https://gist.github.com/GabrielD42/9de9668486ef0b642b48)
+green='\e[0;32m'
+NC='\e[0m' # No Color
+
+usage() {
+	echo -e "Error: Incorrect Arguments.\n./deploy.sh [OPTION] 'commit message'\nWhere [OPTION] is either:\n-g\tGabriel's Pi's IP Address\n-k\tKeane's Pi's IP Address"
+	}
+
 if [ $# == 2 ]; then
-	# color variables for output (see https://gist.github.com/GabrielD42/9de9668486ef0b642b48)
-	green='\e[0;32m'
-	NC='\e[0m' # No Color
 
 	echo -e "\n${green}********************************\n* Uploading new code to Github *\n********************************${NC}\n"
 	git add --all
 	git commit -m $1
 	git push
 	echo -e "\n${green}*************************************\n* Pi downloading and compiling code *\n*************************************${NC}\n"
-	ssh pi@192.168.0.51 '~/pullchanges.sh; make clean; make'
+	while getopts ":gk" opt; do
+		case $opt in
+			g)
+				ssh pi@192.168.0.51 '~/pullchanges.sh; make clean; make'
+				;;
+			k)
+				#insert ip address here
+				;;
+			\?)
+				usage
+				;;
+		esac
+	done
 else
-	echo "Please pass a commit message"
+	usage
 fi
