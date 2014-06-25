5a28356c62d7e2352f9252d35f153a34a1884e2a
HelloKitty7 <thisaintforuruse@gmail.com>
2014-06-24 19:34:52 -0700

    markdown

--- README.md
+++ README.md
@@ -2,3 +2,9 @@ BEN
 ===
 
 The code controlling the Raspberry Pi on my robot, BEN (Bright Enough to Navigate)
+
+
+##square
+* number of times scanned
+* number of times object found here
+* linked list of coordinates that this square has been scanned from
--- ping/ping.cpp
+++ ping/ping.cpp
@@ -20,21 +20,20 @@ long Ping::getCentimeters() {
 	return getReading() / 29 / 2;
 	}
 
-long Ping::getReading() {
-	long duration, inches, cm;
+long Ping::getReading(  n  long duration, inches, cm;
 
-  // The PING))) is triggered by a HIGH pulse of 2 or more microseconds.
-  // Give a short LOW pulse beforehand to ensure a clean HIGH pulse:
-  digitalWrite(triggerPin, LOW);
-  delayMicroseconds(2);
-  digitalWrite(triggerPin, HIGH);
-  delayMicroseconds(5);
-  digitalWrite(triggerPin, LOW);
+	// The PING))) is triggered by a HIGH pulse of 2 or more microseconds.
+	// Give a short LOW pulse beforehand to ensure a clean HIGH pulse:
+	digitalWrite(triggerPin, LOW);
+	delayMicroseconds(2);
+	digitalWrite(triggerPin, HIGH);
+	delayMicroseconds(5);
+	digitalWrite(triggerPin, LOW);
 
-  // The echo pin is used to read the signal from the PING))): a HIGH
-  // pulse whose duration is the time (in microseconds) from the sending
-  // of the ping to the reception of its echo off of an object.
-  duration = utils::pulseIn(echoPin, HIGH, 100000L);
+	// The echo pin is used to read the signal from the PING))): a HIGH
+	// pulse whose duration is the time (in microseconds) from the sending
+	// of the ping to the reception of its echo off of an object.
+	duration = utils::pulseIn(echoPin, HIGH, 100000L);
 
-  return duration;
+	return duration;
 	}
