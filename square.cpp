f45e06cc61db8d11c561b506df27f3e8ee2eb3b4
Gabriel Damon <gabrieldamon42@gmail.com>
2014-06-29 11:47:25 -0700

    debugging

--- square.cpp
+++ square.cpp
@@ -11,6 +11,7 @@ Square::Square(int tempX, int tempY) : x(tempX), y(tempY), scannedFrom(scannedFr
 void Square::scanned(bool found, Square* position) {
 	Node<Square>* newNode = new Node<Square>(position); // allocated on heap
 	//scannedFrom.add(newNode); // list takes over control of newNode
+	delete newNode;
 	numTimesScanned++;
 	if(found) {
 		numTimesFound++;
