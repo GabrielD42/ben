0d5cd5b9087b3856c50408d00cc02504411ef963
Gabriel Damon <gabrieldamon42@gmail.com>
2014-06-29 10:53:34 -0700

    typo

--- list.h
+++ list.h
@@ -12,7 +12,7 @@ class List {
 		/**
 		 * Default constructer, simply initializes all variables
 		 */
-		List<T>() {
+		List() {
 			size = 0;
 			first = 0;
 			}
@@ -55,7 +55,7 @@ class List {
 		/**
 		 * delete all elements in list
 		 */
-		~List<T>() {
+		~List() {
 			Node<T>* nextValue = first;
 			while(nextValue) {
 				Node<T>* deleteThis = nextValue;
