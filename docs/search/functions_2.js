a33ac290ec5dcad0a5e64850681c5fbb5529e42c
Gabriel Damon <gabrieldamon42@gmail.com>
2014-06-29 11:17:21 -0700

    testing seperaton of template file

--- docs/search/functions_2.js
+++ docs/search/functions_2.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['get',['get',['../classList.html#a1224cbefb9144fcf34822e37a33cc5cd',1,'List']]],
+  ['get',['get',['../classList.html#aba29cc654260eef85da56faaf8100815',1,'List']]],
   ['getcentimeters',['getCentimeters',['../classPing.html#aae84b003ab46fbbc9ac591df3ab61e89',1,'Ping']]],
   ['getdata',['getData',['../classNode.html#a37b3e2a48bce02418894a4e6e3ae2862',1,'Node']]],
   ['getinches',['getInches',['../classPing.html#a683f58ccb6bd5ef4c8ee82f2cf0a1652',1,'Ping']]],
--- list.h
+++ list.h
@@ -12,60 +12,71 @@ class List {
 		/**
 		 * Default constructer, simply initializes all variables
 		 */
-		List() {
-			size = 0;
-			first = 0;
-			}
+		List();
 		/**
 		 * adds the argument to the end of the list. object must be allocated on heap. takes over control of object, ie DO NOT DELETE OBJECT
 		 * @param object the address of the object to be added to the list
 		 */
-		void add(Node<T>* object) {
-			if(length() == 0) {
-				first = object;
-				}
-			else {
-				get(length() - 1)->setNext(object);
-				}
-			size++;
-			}
+		void add(Node<T>* object);
 		/**
 		 * the current length of the list
 		 * @return the list length
 		 */
-		int length() {
-			return size;
-			}
+		int length();
 		/**
 		 * accessor function to get the nth member of the list
 		 *
 		 * @param position the 0-indexed position of the wanted object
 		 * @return pointer to object, or 0 if position to large or small
 		 */
-		Node<T>* get(int position) {
-			if(position < length() && position >= 0) {
-				Node<T>* returnValue = first;
-				for(int i = 0; i < position; i++) {
-					returnValue = returnValue->getNext();
-					}
-				return returnValue;
-				}
-			return 0;
-			}
+		Node<T>* get(int position);
 		/**
 		 * delete all elements in list
 		 */
-		~List() {
-			Node<T>* nextValue = first;
-			while(nextValue) {
-				Node<T>* deleteThis = nextValue;
-				nextValue = deleteThis->getNext();
-				delete deleteThis;
-				}
-			}
+		~List();
 	private:
 		int size;
 		Node<T>* first;
 	};
+
+template <class T>
+List<T>::List() {
+	size = 0;
+	first = 0;
+	}
+template <class T>
+void List<T>::add(Node<T>* object) {
+	if(length() == 0) {
+		first = object;
+		}
+	else {
+		get(length() - 1)->setNext(object);
+		}
+	size++;
+	}
+template <class T>
+int List<T>::length() {
+	return size;
+	}
+template <class T>
+Node<T>* List<T>::get(int position) {
+	if(position < length() && position >= 0) {
+		Node<T>* returnValue = first;
+		for(int i = 0; i < position; i++) {
+			returnValue = returnValue->getNext();
+			}
+		return returnValue;
+		}
+	return 0;
+	}
+template <class T>
+List<T>::~List() {
+	Node<T>* nextValue = first;
+	while(nextValue) {
+		Node<T>* deleteThis = nextValue;
+		nextValue = deleteThis->getNext();
+		delete deleteThis;
+		}
+	}
 
 #endif
