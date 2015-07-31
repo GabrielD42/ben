09d71f2ca4f3917ed0efdbf5326811478d957618
Gabriel Damon <gabrieldamon42@gmail.com>
2015-07-31 12:20:41 -0700

    testing

--- robot/bump_test.rb
+++ robot/bump_test.rb
@@ -1,14 +1,9 @@
 require 'pi_piper'
-include PiPiper
 
-watch :pin => 17 do
-  puts "Pin changed from #{last_value} to #{value}"
+puts "Press the switch to get started"
+
+PiPiper.watch :pin => 17, :invert => true do |pin|
+  puts "Pin changed from #{pin.last_value} to #{pin.value}"
 end
-
-#Or
-
-# after :pin => 17, :goes => :high do
-#   puts "Button pressed"
-# end
 
 PiPiper.wait
