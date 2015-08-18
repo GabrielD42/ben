066757d2def2b736db56c9dea24935b3c0832e3c
Gabriel Damon <gabrieldamon42@gmail.com>
2015-08-18 11:41:50 -0700

    testing

--- robot/bump_test.rb
+++ robot/bump_test.rb
@@ -2,8 +2,12 @@ require 'pi_piper'
 
 puts "Watching pin 17..."
 
-PiPiper.watch pin: 17 do |pin|
-  puts "Pin changed from #{pin.last_value} to #{pin.value}"
-end
+# PiPiper.watch pin: 17 do |pin|
+#   puts "Pin changed from #{pin.last_value} to #{pin.value}"
+# end
 
-PiPiper.wait
+# PiPiper.wait
+
+after pin: 17, goes: :high do
+  puts "Button pressed"
+end
