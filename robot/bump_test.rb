c94f8251cdc5fde2791f56b11469d353d29e6715
Gabriel Damon <gabrieldamon42@gmail.com>
2015-08-18 12:03:07 -0700

    testing

--- robot/bump_test.rb
+++ robot/bump_test.rb
@@ -6,8 +6,26 @@ puts "Watching pin 17..."
 #   puts "Pin changed from #{pin.last_value} to #{pin.value}"
 # end
 
-PiPiper.after pin: 17, goes: :high do
-  puts "Button pressed"
+# PiPiper.after pin: 17, goes: :high do
+#   puts "Button pressed"
+# end
+
+def watch(options, &block)
+  new_thread = Thread.new do
+    pin = PiPiper::Pin.new(options)
+    pin.wait_for_change
+    if block.arity > 0
+      block.call pin
+    else
+      pin.instance_exec &block
+    end
+  end
+  new_thread.abort_on_exception = true
+  new_thread
+end
+
+watch pin: 17 do |pin|
+  puts "Pin changed from #{pin.last_value} to #{pin.value}"
 end
 
 PiPiper.wait
