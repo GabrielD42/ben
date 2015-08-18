9189f48d7707351d457e05ccc79cdd316291bcbc
Gabriel Damon <gabrieldamon42@gmail.com>
2015-08-18 12:09:01 -0700

    testing

--- robot/bump_test.rb
+++ robot/bump_test.rb
@@ -10,22 +10,35 @@ puts "Watching pin 17..."
 #   puts "Button pressed"
 # end
 
-def watch(options, &block)
-  new_thread = Thread.new do
-    pin = PiPiper::Pin.new(options)
-    pin.wait_for_change
-    if block.arity > 0
-      block.call pin
-    else
-      pin.instance_exec &block
+module PiPiper
+  def wait_for_change(options, &block)
+    new_thread = Thread.new do
+      pin = PiPiper::Pin.new(options)
+      pin.wait_for_change
+      if block.arity > 0
+        block.call pin
+      else
+        pin.instance_exec &block
+      end
     end
+    new_thread.abort_on_exception = true
+    new_thread
   end
-  new_thread.abort_on_exception = true
-  new_thread
+
+  def once_after(options, &block)
+    options[:trigger] = options.delete(:goes) == :high ? :rising : :falling
+    wait_for_change options, &block
+  end
 end
 
-watch pin: 17 do |pin|
-  puts "Pin changed from #{pin.last_value} to #{pin.value}"
+# PiPiper.wait_for_change pin: 17 do |pin|
+#   puts "Pin changed from #{pin.last_value} to #{pin.value}"
+# end
+
+PiPiper.once_after pin: 17, goes: :high do
+  puts "Button pressed"
 end
+
+puts "Out of block"
 
 PiPiper.wait
