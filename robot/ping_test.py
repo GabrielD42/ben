f6f379e48f6c34d557863f787f7ad907a1b049c5
Gabriel Damon <gabrieldamon42@gmail.com>
2015-08-31 08:33:16 -0700

    testing

--- robot/ping_test.py
+++ robot/ping_test.py
@@ -30,33 +30,35 @@ GPIO.setup(GPIO_ECHO,GPIO.IN)      # Echo
 # Set trigger to False (Low)
 GPIO.output(GPIO_TRIGGER, False)
 
-while True:
-  # Allow module to settle
-  time.sleep(0.5)
+try:
+  while True:
+    # Allow module to settle
+    time.sleep(0.5)
 
-  # Send 10us pulse to trigger
-  GPIO.output(GPIO_TRIGGER, True)
-  time.sleep(0.00001)
-  GPIO.output(GPIO_TRIGGER, False)
-  start = time.time()
-  while GPIO.input(GPIO_ECHO)==0:
+    # Send 10us pulse to trigger
+    GPIO.output(GPIO_TRIGGER, True)
+    time.sleep(0.00001)
+    GPIO.output(GPIO_TRIGGER, False)
     start = time.time()
+    while GPIO.input(GPIO_ECHO)==0:
+      start = time.time()
 
-  while GPIO.input(GPIO_ECHO)==1:
-    stop = time.time()
+    while GPIO.input(GPIO_ECHO)==1:
+      stop = time.time()
 
-  # Calculate pulse length
-  elapsed = stop-start
+    # Calculate pulse length
+    elapsed = stop-start
 
-  # Distance pulse travelled in that time is time
-  # multiplied by the speed of sound (cm/s)
-  distance = elapsed * 34000
+    # Distance pulse travelled in that time is time
+    # multiplied by the speed of sound (cm/s)
+    distance = elapsed * 34000
 
-  # That was the distance there and back so halve the value
-  distance = distance / 2
+    # That was the distance there and back so halve the value
+    distance = distance / 2
 
-  print "Distance : %.1f" % distance
+    print "Distance : %.1f" % distance
 
+finally:
   # Reset GPIO settings
   GPIO.cleanup()
 
